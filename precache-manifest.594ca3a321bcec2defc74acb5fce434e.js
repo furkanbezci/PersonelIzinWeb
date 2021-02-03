287bbe638a59e800ea1df5871867075d5df5c0ab
furkanbezci <72627004+furkanbezci@users.noreply.github.com>
2021-02-03 23:12:59 +0300

    Updates

---
+++ precache-manifest.594ca3a321bcec2defc74acb5fce434e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55940a8928b4f65b0c2042aa6f27c7cd",
+    "url": "/furkanbezci/PersonelIzinWeb/index.html"
+  },
+  {
+    "revision": "16df613d2bd3d24da174",
+    "url": "/furkanbezci/PersonelIzinWeb/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "77beb0000d538c37e740",
+    "url": "/furkanbezci/PersonelIzinWeb/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "16df613d2bd3d24da174",
+    "url": "/furkanbezci/PersonelIzinWeb/static/js/2.940a60a0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/furkanbezci/PersonelIzinWeb/static/js/2.940a60a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77beb0000d538c37e740",
+    "url": "/furkanbezci/PersonelIzinWeb/static/js/main.66819dd3.chunk.js"
+  },
+  {
+    "revision": "962fd68a7c80394618ef",
+    "url": "/furkanbezci/PersonelIzinWeb/static/js/runtime-main.a4fb7496.js"
+  }
+]);
